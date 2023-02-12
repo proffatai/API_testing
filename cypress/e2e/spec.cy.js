40c87abf627c1c57b2533498af507ffdcdde64b2
proffatai <proffatai@gmail.com>
2023-02-12 11:04:22 +0100

    filtered the reponse to be stored

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   let accessToken ='ODJiYzU2MTJkMWI5ODlhOGZhOTdmZGViMGQxMTAwYmVmZTVhNWFiNGUxZTIxMDA3NzYwMzMxODYyNjgzN2Y3MC8vLy8vLzE2NjIwMTUyNDk3Njc=';
+  let xtag='NzI0YzkyMDUyMjg0NTA1NDQxZTkwY2JjNzhiMGZiNDg4YWI5MzA5NzQ3Y2JjZDRlMWQzYWM1MzFjYzc4Yzc4Ny8vLy8vLzE2NjAyMTEzNDc1OTk='
   it('GET Request',()=>{
     for (let i=1; i<=10; i++)
     {
@@ -8,12 +9,15 @@ describe('template spec', () => {
        url:'https://api.veendhq.com/loans/schedule/'+i,
        headers:{
         'Authorization': 'Bearer ' +accessToken, 
-        'x-tag': 'NzI0YzkyMDUyMjg0NTA1NDQxZTkwY2JjNzhiMGZiNDg4YWI5MzA5NzQ3Y2JjZDRlMWQzYWM1MzFjYzc4Yzc4Ny8vLy8vLzE2NjAyMTEzNDc1OTk='
+        'x-tag': xtag
       },
     }).then(response=>{
-        expect(response.status).to.be.equal(200)
-        cy.writeFile('result.json', response.body,{flag: "a+"})
+        if(expect(response.status).to.be.equal(200))
+        {
+        cy.writeFile('result.json', response.body.data.user,{flag: "a+"})
+        cy.writeFile('result.json', response.body.data.schedule,{flag: "a+"})
         cy.writeFile('result.json', '\n\n\n',{flag: "a+"})
+      }
     })
   }
 })
